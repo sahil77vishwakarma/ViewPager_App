c459bc921f460886ddda2b447292bfd476f760a4
Sahil <sahil777vishwakarma@gmail.com>
2023-04-21 11:09:51 +0530

    jai mata di

---
+++ app/src/main/java/com/example/viewpager_app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.viewpager_app
+
+import PageAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        viewPager.adapter = PageAdapter(supportFragmentManager)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        tabLayout.setupWithViewPager(viewPager)
+
+    }
+}
